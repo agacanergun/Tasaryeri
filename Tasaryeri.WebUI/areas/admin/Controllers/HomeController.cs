5c40fd4addfe905808c9b3a9663e91e7ecdd1f74
Ağacan <agcannn@gmail.com>
2023-05-27 16:34:11 +0300

    update

---
+++ Tasaryeri.WebUI/areas/admin/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Tasaryeri.WebUI.areas.admin.Controllers
+{
+    [Area("admin"), Authorize]
+    public class HomeController : Controller
+    {
+        [AllowAnonymous]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
