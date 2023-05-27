3e3591debae7450a54ddd34986407d6bbb4cebad
Ağacan <agcannn@gmail.com>
2023-05-28 02:51:15 +0300

    update
    created core layer crud interface repository and dal layer sqlrepository

---
+++ Tasaryeri.Core/Interfaces/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasaryeri.Core.Interfaces
+{
+    public interface IRepository<T>
+    {
+        public IQueryable<T> GetAll();
+        public IQueryable<T> GetAll(Expression<Func<T, bool>> exp);
+        public T GetBy(Expression<Func<T, bool>> exp);
+        public void Add(T entity);
+        public void Update(T entity);
+        public void Delete(T entity);
+    }
+}
---
+++ Tasaryeri.DAL/Repositories/SqlRepository.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Tasaryeri.Core.Interfaces;
+using Tasaryeri.DAL.Contexts;
+
+namespace Tasaryeri.DAL.Repositories
+{
+    public class SqlRepository<T> : IRepository<T> where T : class
+    {
+
+        SqlContext db;
+        public SqlRepository(SqlContext db)
+        {
+            this.db = db;
+        }
+
+        public void Add(T entity)
+        {
+            db.Add(entity);
+            db.SaveChanges();
+        }
+
+        public void Delete(T entity)
+        {
+            db.Remove(entity);
+            db.SaveChanges();
+        }
+
+        public IQueryable<T> GetAll()
+        {
+            return db.Set<T>();
+        }
+
+        public IQueryable<T> GetAll(Expression<Func<T, bool>> exp)
+        {
+            return db.Set<T>().Where(exp);
+        }
+
+        public T GetBy(Expression<Func<T, bool>> exp)
+        {
+
+            return db.Set<T>().FirstOrDefault(exp);
+
+        }
+
+        public void Update(T entity)
+        {
+            var updatedEntity = db.Entry(entity);
+            updatedEntity.State = EntityState.Modified;
+            db.SaveChanges();
+        }
+    }
+}
