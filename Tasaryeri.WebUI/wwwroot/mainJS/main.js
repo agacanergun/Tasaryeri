884c9fac12bfc241c82265391e18e84d3f9bc875
Ağacan <agcannn@gmail.com>
2023-06-07 15:31:13 +0300

    update

--- Tasaryeri.WebUI/wwwroot/mainJS/main.js
+++ Tasaryeri.WebUI/wwwroot/mainJS/main.js
@@ -493,3 +493,69 @@ function deleteItem(id) {
 
 //Institutional Delet JS
 
+
+
+
+
+
+//Product Delete JS
+
+function confirmDeleteProduct(id) {
+    swal({
+        title: "Silmek İstediğine Emin misin?",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true,
+    }).then((willDelete) => {
+        if (willDelete) {
+            deleteItem(id);
+        }
+    });
+}
+
+function deleteItem(id) {
+    event.preventDefault();
+
+    var data = {
+        id: id
+    };
+
+    $.ajax({
+        url: '/satici/delete',
+        type: 'POST',
+        data: data,
+        success: function (response) {
+            if (response == "Ok") {
+                swal({
+                    title: "Silme İşlemi Başarılı",
+                    icon: "success",
+                    buttons: {
+                        confirm: {
+                            text: "Tamam",
+                            value: true,
+                            visible: true,
+                            className: "",
+                            closeModal: true
+                        }
+                    }
+                }).then((value) => {
+                    if (value) {
+                        location.href = "/satici/satici-ürünleri";
+                    }
+                    else {
+                        location.href = "/satici/satici-ürünleri";
+                    }
+                });
+            } else {
+                alert(response);
+            }
+        },
+        error: function (xhr, status, error) {
+            swal("Error", "An error occurred: " + error, "error");
+        }
+    });
+}
+
+//Product Delet JS
+
+
