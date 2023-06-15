bf997c4a3a1cb3e182219d25204111b8d3e6c915
Ağacan <agcannn@gmail.com>
2023-06-15 10:55:36 +0300

    update

---
+++ Tasaryeri.WebUI/Controllers/MessagesController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Tasaryeri.WebUI.Controllers
+{
+    [Authorize(AuthenticationSchemes = "TasaryeriMemberAuth")]
+    public class MessagesController : Controller
+    {
+        [Route("uye/mesajlar")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
